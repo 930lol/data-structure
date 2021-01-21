e0adf8e49a3f01d0aa9db312c231ef5dfe319a13
930lol <vikashsingh7209482955@gmail.com>
2021-01-21 12:18:27 +0530

    wap in c to print the data of node in the linkedlist

---
+++ printdataofnodde.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+	int data;
+	struct node *link;
+};
+int main()
+{
+	struct node *head=malloc(sizeof(struct node));
+	head->data=98;
+	head->link=NULL;
+	struct node *current=malloc(sizeof(struct node));
+	current->data=38;
+	current->link=NULL;
+	head->link=current;
+	current=malloc(sizeof(struct node));
+	current->data=97;
+	current->link=NULL;
+	head->link->link=current;
+	count_no_node(head);
+	return 0;
+}
+int  count_no_node(struct node *head)
+{
+	int c=0;
+	if(head==NULL)
+	{
+		printf("Linked list is empty");
+	}
+	struct node *ptr=NULL;
+	ptr=head;
+	while(ptr!=NULL)
+	{
+		printf("%d ",ptr->data);
+		ptr=ptr->link;
+	}
+	//return c;
+	//printf("%d",c);
+}
